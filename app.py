9940040789c8415db428f2e17306da6e23944616
Timmyy3000 <59262737+Timmyy3000@users.noreply.github.com>
2021-02-17 10:55:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-
+import numpy as np
 import streamlit as st
-
-
+import cv2
+from streamlit_drawable_canvas import st_canvas
+import pandas as pd
+import random
+from PIL import Image
 SIZE = 252
 
 # HEADER
