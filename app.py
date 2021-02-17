e52d8b3a8306aa7e79ba8271049407f7091f2af0
Timmyy3000 <59262737+Timmyy3000@users.noreply.github.com>
2021-02-17 10:48:56 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import tensorflow
+from tensorflow import keras
+import numpy as np
+import streamlit as st
+import cv2
+from streamlit_drawable_canvas import st_canvas
+import pandas as pd
+import random
+from PIL import Image
+
+SIZE = 252
+
+# HEADER
+st.write('''
+    # Image Processing Neural Network
+    
+    -----
+     
+     This is a Neural Network that analyzes and identifies handwritten data in real time
+     
+     *Click [here](www.google.com), to find my github repository for this project*
+     
+  
+     
+''')
